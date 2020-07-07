56b785787a47b995c4d7fb6eb97ffe643ee10d1c
MajjK <40058179+MajjK@users.noreply.github.com>
2020-07-07 23:38:38 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,260 +1,248 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO; 
 
 namespace Lab1
 {
     public partial class Form1 : Form
     {
+        int OrderNum = 1;
+        int Step = 0;
+
+        Register RegisterA = new Register("A");
+        Register RegisterB = new Register("B");
+        Register RegisterC = new Register("C");
+        Register RegisterD = new Register("D");
+        Dictionary<string, Register> Dict_Register = new Dictionary<string, Register>();
+        Dictionary<string, string> Dict_Part = new Dictionary<string, string>();
         public Form1()
         {
             InitializeComponent();
+            Dict_Register.Add("rAXH", RegisterA);
+            Dict_Register.Add("rAXL", RegisterA);
+            Dict_Register.Add("rBXH", RegisterB);
+            Dict_Register.Add("rBXL", RegisterB);
+            Dict_Register.Add("rCXH", RegisterC);
+            Dict_Register.Add("rCXL", RegisterC);
+            Dict_Register.Add("rDXH", RegisterD);
+            Dict_Register.Add("rDXL", RegisterD);
+            Dict_Part.Add("rAXH", "H");
+            Dict_Part.Add("rAXL", "L");
+            Dict_Part.Add("rBXH", "H");
+            Dict_Part.Add("rBXL", "L");
+            Dict_Part.Add("rCXH", "H");
+            Dict_Part.Add("rCXL", "L");
+            Dict_Part.Add("rDXH", "H");
+            Dict_Part.Add("rDXL", "L");
         }
 
-        int Nr_rozkazu = 1;
-        int Krok = 0;
 
-        Rejestr rejestrA = new Rejestr("A");
-        Rejestr rejestrB = new Rejestr("B");
-        Rejestr rejestrC = new Rejestr("C");
-        Rejestr rejestrD = new Rejestr("D");
-
-
-        private void Rozkaz_Click(object sender, EventArgs e)
+        private void Order_Click(object sender, EventArgs e)
         {
+            // Main function that translates and writes command to the textbox
             bool RadioButtonsChecked = false, RadioRozkazChecked = false, RadioRejestr1Checked = false, RadioRejestr2Checked = false;
 
-            foreach (RadioButton RadioRozkaz in groupBoxRozkaz.Controls.OfType<RadioButton>())
+            foreach (RadioButton RadioRozkaz in groupBoxOrder.Controls.OfType<RadioButton>())
                 if (RadioRozkaz.Checked)
                 {
                     RadioRozkazChecked = true;
                     break;
                 }
-
-            foreach (RadioButton RadioRejestr1 in groupBoxRejestr1.Controls.OfType<RadioButton>())
+            foreach (RadioButton RadioRejestr1 in groupBoxRegister1.Controls.OfType<RadioButton>())
                 if (RadioRejestr1.Checked)
                 {
                     RadioRejestr1Checked = true;
                     break;
                 }
-
-            foreach (RadioButton RadioRejestr2 in groupBoxRejestr1.Controls.OfType<RadioButton>())
+            foreach (RadioButton RadioRejestr2 in groupBoxRegister1.Controls.OfType<RadioButton>())
                 if (RadioRejestr2.Checked)
                 {
                     RadioRejestr2Checked = true;
                     break;
                 }
 
+
             if (RadioRozkazChecked && RadioRejestr1Checked && RadioRejestr2Checked)
                 RadioButtonsChecked = true;
 
-
             if (RadioButtonsChecked)
             {
-                string Rozkaz, Rozkaz_0 = "", RozkazRej1 ="", RozkazRej2 = "";
+                string Order, Order_0 = "", OrderReg1 ="", OrderReg2 = "";
                 bool Rej2_digit = false;
-                Rozkaz = Nr_rozkazu.ToString();
+                Order = OrderNum.ToString();
 
                 if (radioButtonADD.Checked)
-                    Rozkaz_0 += "ADD";
+                    Order_0 += "ADD";
                 else if (radioButtonMOV.Checked)
-                    Rozkaz_0 += "MOV";
+                    Order_0 += "MOV";
                 else if (radioButtonSUB.Checked)
-                    Rozkaz_0 += "SUB";
+                    Order_0 += "SUB";
 
                 if (ButtonRej1AH.Checked)
-                    RozkazRej1 = "rAXH";
+                    OrderReg1 = "rAXH";
                 else if (ButtonRej1AL.Checked)
-                    RozkazRej1 = "rAXL";
+                    OrderReg1 = "rAXL";
                 else if (ButtonRej1BH.Checked)
-                    RozkazRej1 = "rBXH";
+                    OrderReg1 = "rBXH";
                 else if (ButtonRej1BL.Checked)
-                    RozkazRej1 = "rBXL";
+                    OrderReg1 = "rBXL";
                 else if (ButtonRej1CH.Checked)
-                    RozkazRej1 = "rCXH";
+                    OrderReg1 = "rCXH";
                 else if (ButtonRej1CL.Checked)
-                    RozkazRej1 = "rCXL";
+                    OrderReg1 = "rCXL";
                 else if (ButtonRej1DH.Checked)
-                    RozkazRej1 = "rDXH";
+                    OrderReg1 = "rDXH";
                 else if (ButtonRej1DL.Checked)
-                    RozkazRej1 = "rDXL";
+                    OrderReg1 = "rDXL";
 
                 if (ButtonRej2AH.Checked)
-                    RozkazRej2 = "rAXH";
+                    OrderReg2 = "rAXH";
                 else if (ButtonRej2AL.Checked)
-                    RozkazRej2 = "rAXL";
+                    OrderReg2 = "rAXL";
                 else if (ButtonRej2BH.Checked)
-                    RozkazRej2 = "rBXH";
+                    OrderReg2 = "rBXH";
                 else if (ButtonRej2BL.Checked)
-                    RozkazRej2 = "rBXL";
+                    OrderReg2 = "rBXL";
                 else if (ButtonRej2CH.Checked)
-                    RozkazRej2 = "rCXH";
+                    OrderReg2 = "rCXH";
                 else if (ButtonRej2CL.Checked)
-                    RozkazRej2 = "rCXL";
+                    OrderReg2 = "rCXL";
                 else if (ButtonRej2DH.Checked)
-                    RozkazRej2 = "rDXH";
+                    OrderReg2 = "rDXH";
                 else if (ButtonRej2DL.Checked)
-                    RozkazRej2 = "rDXL";
+                    OrderReg2 = "rDXL";
                 else if (ButtonRej2None.Checked)
                 {
-                    RozkazRej2 = numericUpDownLiczba.Value.ToString() + " ";
+                    OrderReg2 = numericUpDownValue.Value.ToString() + " ";
                     Rej2_digit = true;
                 }
 
-
-                if (Rozkaz_0 == "MOV" && Rej2_digit == true)
-                    Console.WriteLine("Błędny Rozkaz");
+                if (Order_0 == "MOV" && Rej2_digit == true)
+                    Console.WriteLine("Wrong Command !");
                 else
                 {
-                    textBoxKod.Text += Rozkaz + " " + Rozkaz_0 + " " + RozkazRej1 + " " + RozkazRej2 + "\r\n";
-                    Nr_rozkazu++;
+                    textBoxCode.Text += Order + " " + Order_0 + " " + OrderReg1 + " " + OrderReg2 + "\r\n";
+                    OrderNum++;
                 }
             }
         }
 
-
-        private void buttonZapisz_Click(object sender, EventArgs e)
+        private void buttonSave_Click(object sender, EventArgs e)
         {
-            System.IO.File.WriteAllText(@"C:\Users\Kacper Maj\Desktop\Programowanie\C#\1\Lab1\KodProgramu.txt", textBoxKod.Text);
+            //Save your program to txt file
+            var GetDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            System.IO.File.WriteAllText(@GetDirectory+"\\ProgramCode.txt", textBoxCode.Text);
         }
 
-        private void buttonWczytaj_Click(object sender, EventArgs e)
+        private void buttonLoad_Click(object sender, EventArgs e)
         {
-            rejestrA = new Rejestr("A");
-            rejestrB = new Rejestr("B");
-            rejestrC = new Rejestr("C");
-            rejestrD = new Rejestr("D");
-            Krok = 0;
+            //Load your program from txt file
+            var GetDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            RegisterA = new Register("A");
+            RegisterB = new Register("B");
+            RegisterC = new Register("C");
+            RegisterD = new Register("D");
+            Step = 0;
 
-            textBoxRejestr.Text = "";
-            textBoxRejestr.Text += "Rejestr " + rejestrA.ZwrocNazwe() + " " + rejestrA.ZwrocWartosc().ToString() + "\r\n"
-               + "Rejestr " + rejestrB.ZwrocNazwe() + " " + rejestrB.ZwrocWartosc().ToString() + "\r\n"
-               + "Rejestr " + rejestrC.ZwrocNazwe() + " " + rejestrC.ZwrocWartosc().ToString() + "\r\n"
-               + "Rejestr " + rejestrD.ZwrocNazwe() + " " + rejestrD.ZwrocWartosc().ToString() + "\r\n";
-            textBoxAktualnyKrok.Text = (Krok).ToString();
+            textBoxRejegister.Text = "";
+            textBoxRejegister.Text += "Register " + RegisterA.ReturnName() + " " + RegisterA.ReturnValue().ToString() + "\r\n"
+               + "Register " + RegisterB.ReturnName() + " " + RegisterB.ReturnValue().ToString() + "\r\n"
+               + "Register " + RegisterC.ReturnName() + " " + RegisterC.ReturnValue().ToString() + "\r\n"
+               + "Register " + RegisterD.ReturnName() + " " + RegisterD.ReturnValue().ToString() + "\r\n";
+            textBoxCurrentStep.Text = (Step).ToString();
 
 
-            textBoxKod.Text = "";
-            textBoxKod.Text = System.IO.File.ReadAllText(@"C:\Users\Kacper Maj\Desktop\Programowanie\C#\1\Lab1\KodProgramu.txt");
-
-            Nr_rozkazu = textBoxKod.Text.Split('\n').Length;
+            textBoxCode.Text = "";
+            textBoxCode.Text = System.IO.File.ReadAllText(@GetDirectory+"\\ProgramCode.txt");
+            OrderNum = textBoxCode.Text.Split('\n').Length;
         }
-
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            bool PracaKrok = false;
+            // Function that starts program execution 
 
-            if (checkBoxPracaKrokowa.Checked)
-                PracaKrok = true;
+            bool StepWork = false;
+            if (checkBoxStepWork.Checked)
+                StepWork = true;
 
+            Interpreter(Step, StepWork);
+            Step++;
 
-            Interpreter(Krok, PracaKrok, textBoxKod.Text);
-            Krok++;
-
-            textBoxRejestr.Text = "";
-            textBoxRejestr.Text += "Rejestr " + rejestrA.ZwrocNazwe() + " " + rejestrA.ZwrocWartosc().ToString() + "\r\n"
-               + "Rejestr " + rejestrB.ZwrocNazwe() + " " + rejestrB.ZwrocWartosc().ToString() + "\r\n"
-               + "Rejestr " + rejestrC.ZwrocNazwe() + " " + rejestrC.ZwrocWartosc().ToString() + "\r\n"
-               + "Rejestr " + rejestrD.ZwrocNazwe() + " " + rejestrD.ZwrocWartosc().ToString() + "\r\n";
+            textBoxRejegister.Text = "";
+            textBoxRejegister.Text += "Register " + RegisterA.ReturnName() + " " + RegisterA.ReturnValue().ToString() + "\r\n"
+               + "Register " + RegisterB.ReturnName() + " " + RegisterB.ReturnValue().ToString() + "\r\n"
+               + "Register " + RegisterC.ReturnName() + " " + RegisterC.ReturnValue().ToString() + "\r\n"
+               + "Register " + RegisterD.ReturnName() + " " + RegisterD.ReturnValue().ToString() + "\r\n";
         }
 
 
-        void Interpreter(int krok, bool PracaKrok, string RozkazBox)
+        void Interpreter(int Step, bool StepWork)
         {
-            Dictionary<string, Rejestr> Slownik_Rej = new Dictionary<string, Rejestr>();
-            Dictionary<string, string> Slownik_Czesci = new Dictionary<string, string>();
-
-            Slownik_Rej.Add("rAXH", rejestrA);
-            Slownik_Rej.Add("rAXL", rejestrA);
-            Slownik_Rej.Add("rBXH", rejestrB);
-            Slownik_Rej.Add("rBXL", rejestrB);
-            Slownik_Rej.Add("rCXH", rejestrC);
-            Slownik_Rej.Add("rCXL", rejestrC);
-            Slownik_Rej.Add("rDXH", rejestrD);
-            Slownik_Rej.Add("rDXL", rejestrD);
-
-            Slownik_Czesci.Add("rAXH", "H");
-            Slownik_Czesci.Add("rAXL", "L");
-            Slownik_Czesci.Add("rBXH", "H");
-            Slownik_Czesci.Add("rBXL", "L");
-            Slownik_Czesci.Add("rCXH", "H");
-            Slownik_Czesci.Add("rCXL", "L");
-            Slownik_Czesci.Add("rDXH", "H");
-            Slownik_Czesci.Add("rDXL", "L");
-
-
-            string[] lines = textBoxKod.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            //  Function that first translates and interprets commands from textbox
+            //  and then calls the appropriate functions to execute command
+            string[] lines = textBoxCode.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
 
             for (int i = 0; i < lines.Length - 1; i++)
             {
-                    string Rozkaz_0 = "", Rozkaz_1 = "", Rozkaz_2 = "";
-                    int liczba, nr_rozkazu;
+                string Order_0 = "", Order_1 = "", Order_2 = "";
+                int Number, OrderNum;
+                string[] words = lines[i].Split(' ');
+                OrderNum = int.Parse(words[0]);
+                Order_0 = words[1];
+                Order_1 = words[2];
+                Order_2 = words[3];
 
-                    string[] words = lines[i].Split(' ');
+                Register RegisterFirst = Dict_Register[Order_1];
+                string RegisterFirstPart = Dict_Part[Order_1];
 
-                    nr_rozkazu = int.Parse(words[0]);
-                    Rozkaz_0 = words[1];
-                    Rozkaz_1 = words[2];
-                    Rozkaz_2 = words[3];
-
-
-                    Rejestr Rejestr1 = Slownik_Rej[Rozkaz_1];
-                    string Czesc_Rejestr1 = Slownik_Czesci[Rozkaz_1];
-
-
-                    try
-                    {
-                        Rejestr Rejestr2 = Slownik_Rej[Rozkaz_2];
-                        string Czesc_Rejestr2 = Slownik_Czesci[Rozkaz_2];
-                        liczba = Rejestr2.ZwrocWartosc();
-                    }
-                    catch (KeyNotFoundException)
-                    {
-                        liczba = int.Parse(Rozkaz_2);
-                    }
-
-
-                if (PracaKrok == false)
+                try
                 {
-                    if (Rozkaz_0 == "ADD")
-                    {
-                        Rejestr1.Dodaj(liczba, Czesc_Rejestr1);
-                    }
-                    else if (Rozkaz_0 == "MOV")
-                    {
-                        Rejestr1.Przesun(liczba, Czesc_Rejestr1);
-                    }
-                    else if (Rozkaz_0 == "SUB")
-                    {
-                        Rejestr1.Odejmij(liczba, Czesc_Rejestr1);
-                    }
-                    textBoxRejestr.Text = "Rejestr " + Rejestr1.ZwrocNazwe() + " " + Rejestr1.ZwrocWartosc().ToString();
-                    textBoxAktualnyKrok.Text = nr_rozkazu.ToString();
+                    Register RegisterSecond = Dict_Register[Order_2];
+                    string RegisterSecondPart = Dict_Part[Order_2];
+                    Number = RegisterSecond.ReturnValue();
+                }
+                catch (KeyNotFoundException)
+                {
+                    Number = int.Parse(Order_2);
                 }
 
-                else if (PracaKrok == true && krok == i)
+
+                if (StepWork == false)
                 {
-                    if (Rozkaz_0 == "ADD")
+                    if (Order_0 == "ADD")
                     {
-                        Rejestr1.Dodaj(liczba, Czesc_Rejestr1);
+                        RegisterFirst.Add(Number, RegisterFirstPart);
                     }
-                    else if (Rozkaz_0 == "MOV")
+                    else if (Order_0 == "MOV")
                     {
-                        Rejestr1.Przesun(liczba, Czesc_Rejestr1);
+                        RegisterFirst.Move(Number, RegisterFirstPart);
                     }
-                    else if (Rozkaz_0 == "SUB")
+                    else if (Order_0 == "SUB")
                     {
-                        Rejestr1.Odejmij(liczba, Czesc_Rejestr1);
+                        RegisterFirst.Substract(Number, RegisterFirstPart);
                     }
-                    textBoxRejestr.Text = "Rejestr " + Rejestr1.ZwrocNazwe() + " " + Rejestr1.ZwrocWartosc().ToString();
-                    textBoxAktualnyKrok.Text = (krok+1).ToString();
+                    textBoxRejegister.Text = "Register " + RegisterFirst.ReturnName() + " " + RegisterFirst.ReturnValue().ToString();
+                    textBoxCurrentStep.Text = OrderNum.ToString();
+                }
+
+                else if (StepWork == true && Step == i)
+                {
+                    if (Order_0 == "ADD")
+                    {
+                        RegisterFirst.Add(Number, RegisterFirstPart);
+                    }
+                    else if (Order_0 == "MOV")
+                    {
+                        RegisterFirst.Move(Number, RegisterFirstPart);
+                    }
+                    else if (Order_0 == "SUB")
+                    {
+                        RegisterFirst.Substract(Number, RegisterFirstPart);
+                    }
+                    textBoxRejegister.Text = "Register " + RegisterFirst.ReturnName() + " " + RegisterFirst.ReturnValue().ToString();
+                    textBoxCurrentStep.Text = (Step+1).ToString();
                 }
             }
         }
